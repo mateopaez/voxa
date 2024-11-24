240fedc524198f9bdf40e27be14fe7480147b2cd
Mateo Paez <paezball@ualberta.ca>
2024-11-24 03:48:02 -0700

    added vercel speed insights functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Controller from "./components/Controller";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   return (
     <div className="">
       <Controller />
+      <SpeedInsights />
     </div>
   );
 }
