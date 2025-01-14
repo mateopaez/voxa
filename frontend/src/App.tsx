8846a2ad06df82edfea68972f0aff0aaf2d54b3b
Mateo Paez <paezball@ualberta.ca>
2025-01-14 03:50:42 -0700

    added vercel analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Controller from "./components/Controller";
 import { SpeedInsights }  from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="">
       <Controller />
       <SpeedInsights />
+      <Analytics />
     </div>
   );
 }
